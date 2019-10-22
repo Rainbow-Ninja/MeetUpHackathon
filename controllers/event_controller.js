9e3b7cc9f6d6e67531e9f343d6fd1e84b45e8115
Jo Dunham <scaffelpike@yahoo.com.au>
2019-10-22 15:57:37 +1100

    edit and destroy for events

--- controllers/event_controller.js
+++ controllers/event_controller.js
@@ -11,14 +11,40 @@ function make (req, res) {
 
 async function create(req, res) {
     console.log("#################### ", req.body);
-    let { title, date, startTime, endTime, address, details, picture } = req.body;
-    let event = await EventModel.create({ title, date, startTime, endTime, address, details, picture })
+    let { title, date, startTime, endTime, address, details } = req.body;
+    // let { picture } = req.file;
+    let event = await EventModel.create({ title, date, startTime, endTime, address, details })
         .catch(err => res.status(500).send(err));
     res.redirect("/event");
+}
+
+const edit = async (req, res) => {
+    let {id} = req.params;
+    let event = await EventModel.findById(id)
+        .catch(err => res.status(500).send(err))
+    res.render("event/edit", {event})
+}
+
+const update = async (req, res) => {
+    let { id } = req.params
+    let { title, date, startTime, endTime, address, details } = req.body
+    await EventModel.findByIdAndUpdate(id, {title, date, startTime, endTime, address, details })
+        .catch(err => res.status(500).send(err));
+    res.redirect(`/event/${id}`)
+}
+
+const destroy = async (req, res) => {
+    let { id } = req.params
+    await EventModel.findByIdAndDelete(id)
+        .catch(err => res.status(500).send(err));
+    res.redirect('/event');
 }
 
 module.exports = {
     index,
     make,
-    create
+    create,
+    edit,
+    update,
+    destroy
 }
--- routes/event_routes.js
+++ routes/event_routes.js
@@ -5,5 +5,6 @@ const EventController = require("./../controllers/event_controller");
 router.get("/event", EventController.index);
 router.get("/event/new", EventController.make);
 router.post("/event", EventController.create);
+router.get("/event/edit/:id", EventController.edit);
 
 module.exports = router;
